a80d2f958043ca735af2aced0d9188aedf84f18c
bobbybakes <bobby.a.baker@gmail.com>
2018-04-04 23:40:54 -0400

    craigslist fridge scraps

---
+++ fridgeScrape.py
@@ -0,0 +1,26 @@
+from lxml import html
+import requests
+
+cl_url = f"https://columbus.craigslist.org/search/zip?query=fridge"
+page = requests.get(cl_url)
+with requests.session() as s:
+    sites = s.get('https://www.craigslist.org/about/sites')
+    tree = html.fromstring(sites.content)
+    hrefs = tree.xpath('//a[contains(@href, "craig")]/@href')
+
+    fridge_tag = '//p[@class="result-info"]/a/text()'
+    total = 0
+    for site in hrefs:
+        cl_url = f"{site}search/zip?query=fridge"
+        # print(cl_url)
+        # print("###########")
+        page = s.get(cl_url)
+        tree = html.fromstring(page.content)
+        results = tree.xpath(fridge_tag)
+        for r in results:
+            total += 1
+            # print(r)
+            # print("----------")
+
+
+    print(f"Total Number of Free Refridgerators: {total}")
---
+++ listSites.py
@@ -0,0 +1,8 @@
+import requests
+from lxml import html
+
+page = requests.get('https://www.craigslist.org/about/sites')
+tree = html.fromstring(page.content)
+hrefs = tree.xpath('//a[contains(@href, "craig")]/@href')
+
+print(hrefs)
